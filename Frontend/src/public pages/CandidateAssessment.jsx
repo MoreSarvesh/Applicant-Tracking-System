9365502d4fcfe9fce2239ef4e18ff5c5d8eaf3be
MoreSarvesh <sarveshmore1324@gmail.com>
2024-05-26 21:01:15 +0530

    question counter in candidate assessment

--- Frontend/src/public pages/CandidateAssessment.jsx
+++ Frontend/src/public pages/CandidateAssessment.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import Modal from "../components/Modal.component.jsx";
 
 const CandidateAssessment = () => {
   const { id } = useParams();
   const [questions, setQuestions] = useState([]);
+  const [showModal, setShowModal] = useState(false);
   const [cid, setCid] = useState("");
   const [opt, setOpt] = useState("");
   const [asnwers, setAnswers] = useState([]);
   const [counter, setCounter] = useState(0);
+  const [candidateid, setcandidateid] = useState(0);
   useEffect(() => {
     fetch(`http://localhost:5000/candidate/assessment/${id}`, {
       credentials: "include",
@@ -47,66 +51,85 @@ const CandidateAssessment = () => {
       answers: asnwers,
     };
 
-    const response = await fetch("http://localhost:5000/candidate/evaluate", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(submission),
-    });
+    const response = await toast.promise(
+      fetch("http://localhost:5000/candidate/evaluate", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(submission),
+      }),
+      {
+        pending: "Submitting response",
+      }
+    );
 
     if (!response.ok) {
       console.log("Assessment Submission Failed");
       const assessmentSubmissionFailData = await response.json();
       console.log(assessmentSubmissionFailData);
-      return;
+      return toast.error(assessmentSubmissionFailData.error);
     }
     console.log("Assessment Submitted successfully");
     const assessmentSubmissionData = await response.json();
     console.log(assessmentSubmissionData);
+    setcandidateid(assessmentSubmissionData.message);
+    setShowModal(true);
+    return;
   };
 
   return (
-    <form className="cassessment" onSubmit={handleCandidateAssessmentSubmit}>
-      <label htmlFor="cid">
-        Applicant's ID:
-        <input
-          type="text"
-          name="cid"
-          id="cid"
-          value={cid}
-          onChange={(e) => setCid(e.target.value)}
-        />
-      </label>
-      <fieldset>
-        <legend>{questions[counter]?.question}</legend>
-        {questions[counter]?.options.map((option) => (
-          <label htmlFor={option} key={option}>
-            <input
-              type="radio"
-              name={questions[counter].question}
-              id={option}
-              value={option}
-              onChange={(e) => setOpt(e.target.value)}
-            />
-            {" " + option}
-          </label>
-        ))}
-      </fieldset>
-      <div className="util-btns">
-        <button
-          onClick={handleClickNext}
-          disabled={counter + 1 > questions?.length - 1}
-        >
-          Next
-        </button>
-        <button onClick={handleClickPrevious} disabled={counter - 1 < 0}>
-          Previous
-        </button>
-        <button type="submit">Submit Assessment</button>
-      </div>
-    </form>
+    <>
+      {" "}
+      <form className="cassessment" onSubmit={handleCandidateAssessmentSubmit}>
+        <label htmlFor="cid">
+          Applicant's ID:
+          <input
+            type="text"
+            name="cid"
+            id="cid"
+            value={cid}
+            onChange={(e) => setCid(e.target.value)}
+          />
+        </label>
+        <div className="cassdiv">
+          {(counter + 1).toString() + "/" + questions?.length}
+          <fieldset>
+            <legend>{questions[counter]?.question}</legend>
+            {questions[counter]?.options.map((option) => (
+              <label htmlFor={option} key={option}>
+                <input
+                  type="radio"
+                  name={questions[counter].question}
+                  id={option}
+                  value={option}
+                  onChange={(e) => setOpt(e.target.value)}
+                />
+                {" " + option}
+              </label>
+            ))}
+          </fieldset>
+        </div>
+        <div className="util-btns">
+          <button
+            onClick={handleClickNext}
+            disabled={counter + 1 > questions?.length - 1}
+          >
+            Next
+          </button>
+          <button onClick={handleClickPrevious} disabled={counter - 1 < 0}>
+            Previous
+          </button>
+          <button type="submit">Submit Assessment</button>
+        </div>
+      </form>
+      {showModal && (
+        <Modal title="Successfully Attempted" setShowModal={setShowModal}>
+          {candidateid}
+        </Modal>
+      )}
+    </>
   );
 };
 
